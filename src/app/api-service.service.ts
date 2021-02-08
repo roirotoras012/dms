43bc7df01dbfbc09eb94294808fc17d4d9961d97
roirotoras012 <roirotoras012@gmail.com>
2021-02-08 23:56:24 +0800

    wq

---
+++ src/app/api-service.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+
+  constructor(private http: HttpClient) { }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+
+  sendPostRequest(): Observable<any> {
+    let postData = {
+      "username": "Customer004",
+      "password": "customer004@email.com",
+      "name": "asdasd"
+}
+    return this.http.post("https://localhost/dms/admin/api",postData 
+   
+   
+    
+    
+);
+    
+   
+    
+  }
+
+ 
+
+
+
+
+
+
+}
