cbcea874c9ac616b41707f7091774d380d5110e2
roirotoras012 <roirotoras012@gmail.com>
2021-02-11 10:40:50 +0800

    added features -delete -edit -multiple delete

--- src/app/edit-modal/edit-modal.component.ts
+++ src/app/edit-modal/edit-modal.component.ts
@@ -1,20 +1,121 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
+import { ToastController, NavParams } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-edit-modal',
   templateUrl: './edit-modal.component.html',
   styleUrls: ['./edit-modal.component.scss'],
 })
 export class EditModalComponent implements OnInit {
-
-  constructor(private modalCtrl: ModalController, private http: HttpClient, private router: Router) { }
+  datauser: any = [];
+  usertobeedited: any = []
+  username: string;
+  password: string;
+  name: string;
+  email: string;
+  usertype: string;
+  department: string;
+  
+  constructor(private modalCtrl: ModalController, private http: HttpClient, private router: Router,public toastController: ToastController,  private navParams: NavParams) { }
   dismissModal(){
     this.modalCtrl.dismiss();
    
   }
-  ngOnInit() {}
+  ngOnInit() { this.getUser()}
+
+  async getUser() {
+ 
+    // this.http.get("https://localhost/dms/admin/getuser") 
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     this.datauser = res;
+    // console.log(this.datauser);
+ 
+    
+    //     }
+      
+    //   , err => {
+    //     console.log(err);
+    //   });
+   
+    this.usertobeedited = this.navParams.get('data');
+    console.log(this.usertobeedited)
+    this.username = this.usertobeedited.username;
+    this.email = this.usertobeedited.email;
+    this.name = this.usertobeedited.name;
+    
+     
+
+
+  }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'User has been successfully updated',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async edit_account() {
+ 
+    
+    if(this.username && this.password && this.name &&  this.email &&  this.usertype &&  this.department != null){
+
+      
+      let postData = {
+        username: this.username,
+        password: this.password,
+        name: this.name,
+        email: this.email,
+        usertype: this.usertype,
+        department_id: this.department,
+        user_id: this.usertobeedited.user_id
+        
+        
+  }
+      this.http.post("https://localhost/dms/admin/edit_user", JSON.stringify(postData)) 
+        .subscribe(res => {
+         
+          console.log(res);
+          this.modalCtrl.dismiss();
+          
+           this.presentToast();
+         
+           this.router.navigateByUrl('admin', {skipLocationChange: true}).then(()=>
+       
+       
+          this.router.navigate(['../manage-users'])
+      
+       
+  
+  
+  
+  
+  );
+        
+    }, err => {
+      console.log(err);
+    });
+  
+  
+  
+  
+    }else{
+      const toast = await this.toastController.create({
+        message: 'Invalid Entries',
+        duration: 2000
+      });
+      toast.present();
+      
+
+    }
+
+
+  }
+  
 
 }
