2269fe70c1e6bfabfba296bb4c947ac49b9dce26
roirotoras012 <roirotoras012@gmail.com>
2021-03-16 03:38:06 +0800

    banoobs

--- src/app/auditor/auditor.page.ts
+++ src/app/auditor/auditor.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from '../components/popover/popover.component';
+import { AccPopoverComponent } from '../components/acc-popover/acc-popover.component';
 @Component({
   selector: 'app-auditor',
   templateUrl: './auditor.page.html',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuditorPage implements OnInit {
 
-  constructor() { }
+  constructor(private popover: PopoverController) { }
 
   ngOnInit() {
+  }
+  async _popOver(ev:any){
+    const popover = await this.popover.create({
+      component: PopoverComponent,
+      event: ev
+    })
+    return await popover.present()
+  }
+
+  async acc_popover(ev:any){
+    const popover = await this.popover.create({
+      component: AccPopoverComponent,
+      event: ev
+    })
+    return await popover.present()
   }
 
 }
